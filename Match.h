fbb90433429319696e383c6e47090ca0660023c3
Nebil Kriedi <nkriedi@gmail.com>
2012-07-21 16:19:37 +0200

    created match model

---
+++ Match.h
@@ -0,0 +1,16 @@
+//
+//  Match.h
+//  tabomb
+//
+//  Created by Nebil Kriedi on 21/07/12.
+//  Copyright (c) 2012 5Knot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Match : NSObject
+
+@property (strong, nonatomic) NSString *identifier;
+@property (strong, nonatomic) NSDate *createdAt;
+
+@end
---
+++ Match.m
@@ -0,0 +1,16 @@
+//
+//  Match.m
+//  tabomb
+//
+//  Created by Nebil Kriedi on 21/07/12.
+//  Copyright (c) 2012 5Knot. All rights reserved.
+//
+
+#import "Match.h"
+
+@implementation Match
+
+@synthesize identifier = _identifier;
+@synthesize createdAt = _createdAt;
+
+@end
