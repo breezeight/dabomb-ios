2e4ef6964b9836b07963d09870601513fae00bce
Nebil Kriedi <nkriedi@gmail.com>
2012-07-21 17:21:58 +0200

    created BombWire model

---
+++ BombWire.m
@@ -0,0 +1,17 @@
+//
+//  BombWire.m
+//  tabomb
+//
+//  Created by Nebil Kriedi on 21/07/12.
+//  Copyright (c) 2012 5Knot. All rights reserved.
+//
+
+#import "BombWire.h"
+
+@implementation BombWire
+
+@synthesize colorName = _colorName;
+@synthesize color = _color;
+@synthesize cut = _cut;
+
+@end
